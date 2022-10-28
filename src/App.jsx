a0421fbbcdb854bdd8f6033c4dfa601ffdc7dc48
Esteban <esteban.ouvre@gmail.com>
2022-10-28 16:53:13 +0200

    Rooting ready

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Topbar from "./scenes/global/topBar"
 import SideBar from "./scenes/global/sideBar"
 import {Routes, Route} from "react-router-dom"
-import Dashboard from "./scenes/dashboard/index"
+import Dashboard from "./scenes/dashboard"
+// import Team from "./scenes/team"
+// import Invoice from "./scenes/invoice"
+// import Contacts from "./scenes/contacts"
+// import Bar from "./scenes/bar"
+// import Form from "./scenes/form"
+// import Line from "./scenes/line"
+// import Pie from "./scenes/pie"
+// import FAQ from "./scenes/faq"
+// import Geography from "./scenes/geography"
+// import Calendar from "./scenes/calendar"
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -14,10 +24,21 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/invoice" element={<Invoice />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
             </Routes>
           </main>
         </div>
