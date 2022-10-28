601184159e50c378bd40dbe97a998eefe3b1ef41
Esteban <esteban.ouvre@gmail.com>
2022-10-28 15:00:36 +0200

    Theming with MUI / App config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseLine, ThemeProvider } from "@mui/material";
 
 function App() {
-  return (
+  const [theme, colorMode] = useMode();
 
-  <div className="app">
-    </div>
-  )
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseLine />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
-export default App
+export default App;
